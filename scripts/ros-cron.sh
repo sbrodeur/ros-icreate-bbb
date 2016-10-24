c56dd0f573f37f17367093927488d8260603b46b
Simon Brodeur <Simon Brodeur@USherbrooke.ca>
2016-10-24 11:30:15 -0400

    Update launch scripts for recording and realtime mode

--- scripts/ros-cron.sh
+++ scripts/ros-cron.sh
@@ -4,13 +4,17 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 now=$(date +"%T")
 
+echo "[$now] Configure Quickcam Pro 9000 cameras disabled"
+exit 0
+
+
 if [ -e "/dev/video6" ]
 then
 	# Configure Quickcam Pro 9000 camera (left)
 	echo "[$now] Configuring Quickcam Pro 9000 camera (left)"
 	v4l2-ctl --device=/dev/video6 --set-ctrl exposure_auto=1 # Manual Mode
 	v4l2-ctl --device=/dev/video6 --set-ctrl backlight_compensation=0
-	v4l2-ctl --device=/dev/video6 --set-ctrl exposure_absolute=1000
+	v4l2-ctl --device=/dev/video6 --set-ctrl exposure_absolute=500
 	v4l2-ctl --device=/dev/video6 --set-ctrl gain=255
 	v4l2-ctl --device=/dev/video6 --set-ctrl focus=0
 	v4l2-ctl --device=/dev/video6 --set-ctrl white_balance_temperature_auto=0
@@ -26,7 +30,7 @@ then
 	echo "[$now] Configuring Quickcam Pro 9000 camera (right)"
 	v4l2-ctl --device=/dev/video7 --set-ctrl exposure_auto=1 # Manual Mode
 	v4l2-ctl --device=/dev/video7 --set-ctrl backlight_compensation=0
-	v4l2-ctl --device=/dev/video7 --set-ctrl exposure_absolute=1000
+	v4l2-ctl --device=/dev/video7 --set-ctrl exposure_absolute=500
 	v4l2-ctl --device=/dev/video7 --set-ctrl gain=255
 	v4l2-ctl --device=/dev/video7 --set-ctrl focus=0
 	v4l2-ctl --device=/dev/video7 --set-ctrl white_balance_temperature_auto=0
--- scripts/ros.sh
+++ scripts/ros.sh
@@ -9,8 +9,8 @@ xboxdrv --mimic-xpad-wireless --priority=normal --silent &
 sleep 4
 
 # Start roscore instance
-export ROS_MASTER_URI=http://192.168.4.1:11311
-export ROS_IP=192.168.4.1
+export ROS_MASTER_URI=http://192.168.7.2:11311
+export ROS_IP=192.168.7.2
 export ROS_PORT=11311
 
 source /opt/ros/kinetic/setup.bash
@@ -19,13 +19,13 @@ source $CATKIN_WORKSPACE_DIR/devel/setup.bash
 /opt/ros/kinetic/bin/roscore -p $ROS_PORT &
 sleep 4
 
-# Configure barometer and temperature sensor (bmp180)
+# Configure barometer and temperature sensor (bmp180) for standard accuracy (faster sampling)
 echo 1 > /sys/bus/i2c/drivers/bmp085/2-0077/oversampling
 
 # Configure Quickcam Pro 9000 camera (left)
 v4l2-ctl --device=/dev/video6 --set-ctrl exposure_auto=1 # Manual Mode
 v4l2-ctl --device=/dev/video6 --set-ctrl backlight_compensation=0
-v4l2-ctl --device=/dev/video6 --set-ctrl exposure_absolute=1000
+v4l2-ctl --device=/dev/video6 --set-ctrl exposure_absolute=500
 v4l2-ctl --device=/dev/video6 --set-ctrl gain=255
 v4l2-ctl --device=/dev/video6 --set-ctrl power_line_frequency=2 # 60 Hz
 v4l2-ctl --device=/dev/video6 --set-ctrl focus=0
@@ -36,7 +36,7 @@ v4l2-ctl --device=/dev/video6 --set-ctrl led1_mode=0 # Off
 # Configure Quickcam Pro 9000 camera (right)
 v4l2-ctl --device=/dev/video7 --set-ctrl exposure_auto=1 # Manual Mode
 v4l2-ctl --device=/dev/video7 --set-ctrl backlight_compensation=0
-v4l2-ctl --device=/dev/video7 --set-ctrl exposure_absolute=1000
+v4l2-ctl --device=/dev/video7 --set-ctrl exposure_absolute=500
 v4l2-ctl --device=/dev/video7 --set-ctrl gain=255
 v4l2-ctl --device=/dev/video7 --set-ctrl power_line_frequency=2 # 60 Hz
 v4l2-ctl --device=/dev/video7 --set-ctrl focus=0
@@ -45,4 +45,4 @@ v4l2-ctl --device=/dev/video7 --set-ctrl white_balance_temperature=1000
 v4l2-ctl --device=/dev/video7 --set-ctrl led1_mode=0 # Off
 
 # Start capture nodes
-/opt/ros/kinetic/bin/roslaunch $CATKIN_WORKSPACE_DIR/scripts/ros-nodes.launch
+/opt/ros/kinetic/bin/roslaunch $CATKIN_WORKSPACE_DIR/scripts/ros-nodes-record.launch
