328512acffc00e9a2c346847ce089c34591248ac
Simon Carrier <simongcarrier@gmail.com>
2016-10-28 16:45:57 -0400

    Added madgwick filtering to rosbag dataset creation process

--- src/action/scripts/record/data/create_rosbag_dataset.sh
+++ src/action/scripts/record/data/create_rosbag_dataset.sh
@@ -7,37 +7,44 @@ IFS=$'\n';
 
 # Recursively loop through all bag files in the specified directory
 BAG_DIRECTORY=$1
-for bag in $(find ${BAG_DIRECTORY} -name '*.bag.raw'); do 
+for bag in $(find ${BAG_DIRECTORY} -name '*.bag.raw'); do
 	echo "Processing bag file ${bag}"
-	
+
 	INPUT_DATASET_FILE="${bag}"
-	OUTPUT_DATASET_FILE_BAG_PROC="${bag}.proc"
+	OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED="${bag}_unbatched.proc"
+  OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK="${bag}_madgwick.proc"
 	OUTPUT_DATASET_FILE_BAG="${bag%.bag.raw}.bag"
-	
+
 	if [ -f $OUTPUT_DATASET_FILE_BAG ]; then
 		echo "Output dataset ${OUTPUT_DATASET_FILE_BAG} already found"
 		echo "Skipping conversion for bag file ${INPUT_DATASET_FILE}"
 		continue
 	fi
-	
+
 	echo "Removing any existing dataset temporary files"
-	rm -f $OUTPUT_DATASET_FILE_BAG_PROC $OUTPUT_DATASET_FILE_BAG
-	
-	python ${DIR}/convert_rosbag.py --input=$INPUT_DATASET_FILE --output=$OUTPUT_DATASET_FILE_BAG_PROC
-	if ! [ -f $OUTPUT_DATASET_FILE_BAG_PROC ]; then
-		echo "Could not find temporary file ${OUTPUT_DATASET_FILE_BAG_PROC}. An error probably occured during conversion."
+	rm -f $OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED $OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK $OUTPUT_DATASET_FILE_BAG
+
+	python ${DIR}/convert_rosbag.py --input=$INPUT_DATASET_FILE --output=$OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED
+	if ! [ -f $OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED ]; then
+		echo "Could not find temporary file ${OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED}. An error probably occured during conversion."
 		exit 1
 	fi
-	cp $OUTPUT_DATASET_FILE_BAG_PROC $OUTPUT_DATASET_FILE_BAG
-	
+
+  rosrun imu_filter_madgwick imu_filter_rosbag -i $OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED  -o $OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK
+  if ! [ -f $OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK ]; then
+		echo "Could not find temporary file ${OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK}. An error probably occured during conversion."
+		exit 1
+	fi
+
+	cp $OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK $OUTPUT_DATASET_FILE_BAG
+
 	echo "Compressing rosbag file ${OUTPUT_DATASET_FILE_BAG}"
 	rosbag compress --force --bz2 $OUTPUT_DATASET_FILE_BAG
 	rm -f ${OUTPUT_DATASET_FILE_BAG%.bag}.orig.bag
-	
+
 	echo "Removing all dataset temporary files"
-	rm -f $OUTPUT_DATASET_FILE_BAG_PROC
-	
+  rm -f $OUTPUT_DATASET_FILE_BAG_PROC_UNBATCHED $OUTPUT_DATASET_FILE_BAG_PROC_MADGWICK 
+
 done
 
 echo 'All done.'
-
