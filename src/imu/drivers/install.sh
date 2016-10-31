e716257f676354604b8cc8c2bec03d31425a1a3c
Simon Carrier <simongcarrier@gmail.com>
2016-10-31 10:24:03 -0400

    Install script now unloads and reloads drivers during install

--- src/imu/drivers/install.sh
+++ src/imu/drivers/install.sh
@@ -3,6 +3,13 @@
 # Project directory on local computer
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+rmmod bmp085
+rmmod bmp085-probe
+rmmod l3gd20
+rmmod l3gd20-probe
+rmmod lsm303d
+rmmod lsm303d-probe
+
 cd $DIR/bmp180
 make clean
 make 
@@ -20,5 +27,16 @@ make clean
 make 
 make install 
 depmod -a
+
+
+
+
+modprobe bmp085
+modprobe bmp085-probe
+modprobe l3gd20
+modprobe l3gd20-probe
+modprobe lsm303d
+modprobe lsm303d-probe
+
 
 echo 'All done.'
